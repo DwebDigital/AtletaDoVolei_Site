6860eed3e376f54a69bc3b771c6de0a2e09c5cb1
Helder <helder@dweb.digital>
2023-07-07 09:35:31 -0700

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+const Moving = document.getElementById("moving");
+
+document.onmousemove = ev => {
+  const positionX = (window.innerWidth / -10 - ev.x) / -80;
+  const positionY = -ev.y / 90;
+  Moving.style.transform = `translate(${positionX}px, ${positionY}px)`;
+};
